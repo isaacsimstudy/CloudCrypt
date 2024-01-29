5c4225d5b9313da957793202b9c16f98b5f56d1b
Goh Rui Kun <rkgoh001@mymail.sim.edu.sg>
2024-01-30 04:37:59 +0800

    Added test.html, UserController.java

---
+++ src/main/java/csit321/cloudcrypt/Controller/UserController.java
@@ -0,0 +1,59 @@
+package csit321.cloudcrypt.Controller;
+
+import csit321.cloudcrypt.Entity.UserAccount;
+import csit321.cloudcrypt.Repository.UserAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Controller
+public class UserController {
+
+    private static final Logger logger = Logger.getLogger(UserController.class.getName());
+
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
+    @GetMapping("/createAccount")
+    public String showCreateAccountForm(Model model) {
+        model.addAttribute("userAccount", new UserAccount());
+        return "OwnerAdminAccount";
+    }
+    @GetMapping("/OwnerAdminAccount")
+    public String showOwnerAdminAccount(Model model) {
+        // Logic to prepare data for the "OwnerAdminAccount" view, if needed
+        return "OwnerAdminAccount";
+    }
+
+    @PostMapping("/createAccount")
+    public String createAccount(UserAccount userAccount) {
+        try {
+            logger.info("Received user account data: " + userAccount.toString());
+
+            userAccountRepository.save(userAccount);
+
+            logger.info("User account saved successfully!");
+            alert("Admin account created successfully!");
+            // Redirect to "/OwnerAdminAccount"
+            return "redirect:/OwnerAdminAccount";
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error saving user account: " + e.getMessage(), e);
+            return "error";
+        }
+    }
+
+    private void alert(String s) {
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        logger.info("User account saved successfully!");
+        return "null";
+    }
+
+}
--- src/main/java/csit321/cloudcrypt/Repository/UserAccountRepository.java
+++ src/main/java/csit321/cloudcrypt/Repository/UserAccountRepository.java
@@ -2,8 +2,10 @@ package csit321.cloudcrypt.Repository;
 
 import csit321.cloudcrypt.Entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, UUID> {
 }
